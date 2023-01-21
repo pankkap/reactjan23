c3aa5b7db1656a368d7f79a8e08f36eaea65ce86
pankkap <pankkap@gmail.com>
2023-01-21 16:22:04 +0530

    Session-2

---
+++ ES6 Features/Session-2/2-TemplateString.js
@@ -0,0 +1,7 @@
+var fname = "Pankaj"
+var lname = "kapoor"
+
+
+console.log("Welcome " + fname+ " " + lname + " React Training" + " 40 hours Training")
+
+console.log(`Welcome ${fname} ${lname} React Training 40 Hours Duration`)
---
+++ ES6 Features/Session-2/6-RestOperator.js
@@ -0,0 +1,11 @@
+// Rest Operator ->  It is used to Pack the data 
+
+function add(a,b,...numbers)
+{
+console.log(numbers)
+console.log(a,b)
+}
+add(5,10,20,30,40,50,60);
+
+
+
