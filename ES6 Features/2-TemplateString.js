f85af070cfb639c2ba7b78e9e3d65be042a944ef
pankkap <pankkap@gmail.com>
2023-01-21 16:19:32 +0530

    Session-1

--- ES6 Features/2-TemplateString.js
+++
@@ -1,7 +0,0 @@
-var fname = "Pankaj"
-var lname = "kapoor"
-
-
-console.log("Welcome " + fname+ " " + lname + " React Training" + " 40 hours Training")
-
-console.log(`Welcome ${fname} ${lname} React Training 40 Hours Duration`)
--- ES6 Features/7.SpreadOperator.js
+++
@@ -1,5 +0,0 @@
-// Spread Operator is used unpack the data
-let odd = [1,3,5]
-let merge = [2,4,6, ...odd ]
-
-console.log(merge)
