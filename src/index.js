756d612294c040ca955c44b5bd0aa0b66d45b626
pankkap <pankkap@gmail.com>
2023-01-21 19:49:32 +0530

    Session-4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+// To create React JSX
+import ReactDOM from 'react-dom'
+// To render JSX
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//JSX
+let element  = <h1>Bye Bye HTML, CSS, JS</h1>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Rendering
+ReactDOM.render(element, document.getElementById('root'))
+
